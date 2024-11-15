3f925e7ebb9ddde00dcf61aaa29c3d92e69d1592
aditi <aditi@gmail.com>
2024-11-15 10:03:31 +0530

    Add project files.

---
+++ SecMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
